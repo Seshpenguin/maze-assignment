a91ac7d1ed6039260162a26694144cbcdfa65e04
Seshpenguin <seshan10@me.com>
2018-10-04 14:23:33 -0400

    Stuff

--- src/main/java/net/dolphinbox/maze/Main.java
+++ src/main/java/net/dolphinbox/maze/Main.java
@@ -1,6 +1,5 @@
 package net.dolphinbox.maze;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class Main {
@@ -8,8 +7,7 @@ public class Main {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
 
-        String[][] maze = {
-                { "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B" },
+        String[][] maze = { { "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B" },
                 { "B", "O", "O", "O", "B", "O", "B", "O", "O", "O", "B" },
                 { "B", "O", "B", "O", "B", "O", "B", "O", "B", "B", "B" },
                 { "B", "O", "B", "O", "S", "O", "O", "O", "O", "O", "B" },
@@ -67,48 +65,25 @@ public class Main {
         int hor = startPoint[0][0];
         int ver = startPoint[1][0];
 
+        System.out.println("Called method: " + Arrays.deepToString(startPoint));
+
         System.out.println("Current Position: " + hor + ", " + ver);
 
         int numNodes = 0;
-
-        if (tried[ver + 1][hor]) { // if it's true UP
-            int[][] startPointA = {{hor}, {ver + 1}};
-            numNodes++;
-            if (!computeMaze(maze, tried, startPointA)) {
-                tried[ver + 1][hor] = false;
-                return false;
-            }
-        }
-        if (tried[ver - 1][hor]) { // if it's true DOWM
-            int[][] startPointA = {{hor}, {ver - 1}};
-            numNodes++;
-            if (!computeMaze(maze, tried, startPointA)) {
-                tried[ver - 1][hor] = false;
-                return false;
-            }
-        }
-        if (tried[ver][hor + 1]) { // if it's true RIGHT
-            int[][] startPointA = {{hor}, {ver + 1}};
-            numNodes++;
-            if (!computeMaze(maze, tried, startPointA)) {
-                tried[ver][hor + 1] = false;
-                return false;
-            }
-        }
-        if (tried[ver][hor - 1]) { // if it's true LEFT
-            int[][] startPointA = {{hor}, {ver + 1}};
-            numNodes++;
-            if (!computeMaze(maze, tried, startPointA)) {
-                tried[ver][hor - 1] = false;
-                return false;
-            }
-        }
-
-        if (numNodes < 2) {
-            tried[ver][hor] = false;
+        if (ver >= maze.length - 1 || hor >= maze[0].length - 1) {
             return false;
         }
+        
+        // check the up and down from the current node.
+        int[][] startPointA = {
+            {ver},
+            {hor}
+        };
+        if(computeMaze(maze, tried, startPoint)){
+            
+        }
 
-        return true;
+        return false;
+        
     }
 }
