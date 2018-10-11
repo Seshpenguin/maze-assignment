b3e5ff56ba31feeec40ab85eee5c4cad7ba14671
Kath3x <j2c1c.d1ng.l1@gmail.com>
2018-10-11 15:38:08 -0400

    Random Maze Gne
    Screw spelling

---
+++ src/main/java/net/dolphinbox/maze/RMazeGen.java
@@ -0,0 +1,34 @@
+package net.dolphinbox.maze;
+import java.util.*;
+
+public class RMazeGen {
+    public static Random rand = new Random();
+    public static boolean isDebuggerOn = true;
+
+    public static String[][] randomMazeGen (int verSize, int horSize) {
+        String[][] maze = new String[verSize][horSize];
+        for (int i = 0; i < verSize; i++) {
+            for (int j = 0; j < horSize; j++) {
+                int temp = rand.nextInt(2) + 1;
+                if (temp == 1) {
+                    maze[i][j] = "B";
+                } if (temp == 2) {
+                    maze[i][j] = "O";
+                }
+            }
+        }
+        int verLocalOfS, horLocalOfS, verLocalOfX, horLocalOfX;
+        verLocalOfS = rand.nextInt(verSize - 1);
+        verLocalOfX = rand.nextInt(verSize - 1);
+        horLocalOfS = rand.nextInt(horSize - 1);
+        horLocalOfX = rand.nextInt(horSize - 1);
+
+        maze[verLocalOfS][horLocalOfS] = "S";
+        maze[verLocalOfX][horLocalOfX] = "X";
+
+        if (isDebuggerOn) for (int i = 0; i < verSize; i++) {
+            System.out.println(Arrays.toString(maze[i]));
+        }
+        return maze;
+    }
+}
